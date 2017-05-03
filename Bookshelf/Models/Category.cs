5760206008619afcb02e8f9e16d477f2d9221b2c
lilykrichels <lily.krichels@gmail.com>
2017-05-03 14:54:40 -0400

    First Commit

---
+++ Bookshelf/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Category
+    {
+        [Key]
+        public int  ID { get; set;  }
+        [Display(Name = "Category Name")]
+
+        //this tells the program to change display "Category Name" instead of name on the table
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+        //this is is a type of list that calls from the Book Class and is called "books"
+    }
+}
